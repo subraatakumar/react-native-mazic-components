22e6b2f252e4e6ad5bc6d041d6018b75844b7ca6
subratsir <business.ttrc@gmail.com>
2022-10-12 17:00:52 +0530

    mazic text input changed

--- CustomTextInput.tsx
+++ CustomTextInput.tsx
@@ -1,15 +1,15 @@
-import {View, Text, TextInput} from 'react-native';
-import React, {useState} from 'react';
-import {globalStyles} from './AppCss';
+import { View, Text, TextInput } from "react-native";
+import React, { useState } from "react";
+import { globalStyles } from "./AppCss";
 
 type validations =
-  | 'valid-email'
-  | 'only-digits'
-  | 'only-letters'
-  | 'no-space'
-  | 'max'
-  | 'min'
-  | 'nil';
+  | "valid-email"
+  | "only-digits"
+  | "only-letters"
+  | "no-space"
+  | "max"
+  | "min"
+  | "nil";
 type CustomTextInputProps = {
   value: string;
   setValue: Function;
@@ -20,6 +20,25 @@ type CustomTextInputProps = {
   style?: any;
   max?: number;
   min?: number;
+  m?: number | null;
+  mb?: number;
+  mt?: number;
+  ml?: number;
+  mr?: number;
+  h?: number;
+  w?: number;
+  fct?: string| null; // Font color title
+  fci?: string| null; // Font color input
+  fce?: string| null; // Font color error
+  bgct?: string| null; // Background color title. Same as bgci if not provided
+  bgci?: string| null; // Background color input. Default white
+  bgce?: string| null; // Background color error. Same as bgci if not provided
+  fst?: number; // Font Size title, default is 12
+  fsi?: number; // Font size input, default is 16
+  fse?: number; // Font size error, default is 10
+  title?: string| null; // Same as placeholder if not mentioned
+  hideTitle?: boolean; // if true title will be hiden. Default is false
+  errorPosition?: "default" | "normal";
 };
 
 const CustomTextInput = ({
@@ -27,69 +46,149 @@ const CustomTextInput = ({
   setValue,
   err = false,
   setErr = () => {},
-  placeholder = '',
-  validation = ['nil'],
+  placeholder = "",
+  validation = ["nil"],
   style = [],
   min = 3,
   max = 30,
+  m = null,
+  ml = 0,
+  mr = 0,
+  mb = 0,
+  mt = 0,
+  h = 40,
+  w = 250,
+  fct = null,
+  fce = "red",
+  fci = null,
+  fst = 14,
+  fsi = 16,
+  fse = 10,
+  bgci = "#fff",
+  bgct = null,
+  bgce = null,
+  title = null,
+  hideTitle = false,
+  errorPosition = "default"
 }: CustomTextInputProps) => {
-  const [compErr, setCompErr] = useState('');
+  const [compErr, setCompErr] = useState("");
+  const [pText, setPtext] = useState(title ? " " + title + " " : "");
 
   let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
 
-  const onChangeText = t => {
+  const onChangeText = (t) => {
+    setValue(t);
     // console.log(validation);
+    if (t === "") {
+      setCompErr("");
+      setPtext(title ? " " + title + " " : "");
+      return;
+    } else {
+      setPtext(title ? " " + title + " " : " " + placeholder + " ");
+    }
     for (let i = 0; i < validation.length; i++) {
-      if (validation[i] === 'valid-email' && reg.test(t) === false) {
+      if (validation[i] === "valid-email" && reg.test(t) === false) {
         // console.log(validation[i], ' valid-email error');
-        setCompErr('Invalid Email Address');
+        setCompErr(" Invalid Email Address ");
         !err && setErr(true);
         break;
-      } else if (validation[i] === 'no-space' && t.trim().includes(' ')) {
+      } else if (validation[i] === "no-space" && t.trim().includes(" ")) {
         // console.log(validation[i], ' no-space error');
-        setCompErr("Don't include space");
+        setCompErr(" Don't include space ");
         !err && setErr(true);
         break;
-      } else if (validation[i] === 'only-digits' && isNaN(t)) {
+      } else if (validation[i] === "only-digits" && isNaN(t)) {
         // console.log(validation[i], ' only-digit error');
-        setCompErr("Don't include letters");
+        setCompErr(" Don't include letters ");
         !err && setErr(true);
         break;
-      } else if (validation[i] === 'only-letters' && /[0-9]/.test(t)) {
+      } else if (validation[i] === "only-letters" && /[0-9]/.test(t)) {
         // console.log(validation[i], ' only-letters error');
-        setCompErr("Don't include numbers");
+        setCompErr(" Don't include numbers ");
         !err && setErr(true);
         break;
-      } else if (validation[i] === 'max' && t.trim().length > max) {
+      } else if (validation[i] === "max" && t.trim().length > max) {
         // console.log(validation[i], ' max error');
-        setCompErr('Should not be greater then ' + max + ' characters.');
+        setCompErr(" Should not be greater then " + max + " characters ");
         !err && setErr(true);
         break;
-      } else if (validation[i] === 'min' && t.trim().length < min) {
+      } else if (validation[i] === "min" && t.trim().length < min) {
         // console.log(validation[i], ' min error');
-        setCompErr('Should not be less then ' + min + ' characters.');
+        setCompErr(" Should not be less then " + min + " characters ");
         !err && setErr(true);
         break;
       } else if (validation.length == i + 1) {
         // console.log(validation[i], ' no-error');
-        setCompErr('');
+        setCompErr("");
         setErr(false);
       }
     }
-    setValue(t);
   };
   return (
-    <>
+    <View
+      style={{
+        marginBottom: m ? m : mb,
+        marginTop: m ? m : mt,
+        marginLeft: m ? m : ml,
+        marginRight: m ? m : mr
+      }}
+    >
+      {!hideTitle && (
+        <Text
+          style={{
+            position: "absolute",
+            fontSize: fst,
+            color: fct,
+            top: -10,
+            marginLeft: 10,
+            backgroundColor: bgct ? bgct : bgci,
+            borderRadius: 3
+          }}
+        >
+          {`${pText}`}
+        </Text>
+      )}
       <TextInput
         value={value}
-        onChangeText={t => {
+        onChangeText={(t) => {
           onChangeText(t);
         }}
-        placeholder={placeholder ? placeholder : ''}
-        style={{...globalStyles.planeTextInput, ...style}}
+        placeholder={placeholder ? placeholder : ""}
+        style={{
+          ...globalStyles.planeTextInput,
+          height: h,
+          width: w,
+          fontSize: fsi,
+          backgroundColor: bgci,
+          color: fci,
+          ...style
+        }}
       />
-      <Text style={{color: 'red'}}>{compErr}</Text>
-    </>
+      <Text
+        style={
+          errorPosition === "normal"
+            ? {
+                color: fce,
+                fontSize: fse,
+                backgroundColor: bgce ? bgce : bgci,
+                borderRadius: 3,
+                width: w,
+                marginTop: 3
+              }
+            : {
+                color: fce,
+                fontSize: fse,
+                position: "absolute",
+                top: h - 10,
+                marginLeft: 10,
+                backgroundColor: bgce ? bgce : bgci,
+                borderRadius: 3
+              }
+        }
+      >
+        {compErr}
+      </Text>
+    </View>
   );
 };
 
--- ReadMe.md
+++ ReadMe.md
@@ -2,9 +2,44 @@
 
 This package is created for my personal use, but if you find it helpful, I will be very happy. This package is aimed to provide custom React Native Components.
 
-## Custom Text Input
+## Mazic Text Input
+
+![mazic text input](./src/img/mazictextinput1.jpg)
+
+### Props
+
+-  value: string;
+-  setValue: Function;
+-  err?: boolean;
+-  setErr?: Function;
+-  placeholder?: string;
+-  validation?: validations[];
+-  style?: any;
+-  max?: number;
+-  min?: number;
+-  m?: number | null;
+-  mb?: number;
+-  mt?: number;
+-  ml?: number;
+-  mr?: number;
+-  h?: number;
+-  w?: number;
+-  fct?: string| null; // Font color title
+-  fci?: string| null; // Font color input
+-  fce?: string| null; // Font color error
+-  bgct?: string| null; // Background color title. Same as bgci if not provided
+-  bgci?: string| null; // Background color input. Default white
+-  bgce?: string| null; // Background color error. Same as bgci if not provided
+-  fst?: number; // Font Size title, default is 12
+-  fsi?: number; // Font size input, default is 16
+-  fse?: number; // Font size error, default is 10
+-  title?: string| null; // Same as placeholder if not mentioned
+-  hideTitle?: boolean; // if true title will be hiden. Default is false
+-  errorPosition?: "default" | "normal";
 
 ### Uses Example 
+
+[CodeSandbox Link of working example](https://codesandbox.io/s/mazic-component-example-wkjvbj?file=/src/App.js)
 
 ```js
 function anyFunction(){
@@ -45,9 +80,27 @@ In the above example at the beginning we have set the error to true. Means we ar
 
 ### Validation Details
 
-- no-space: the input can not contain spaces
-- only-digits: the input can contain only numbers from 0 to 9
-- only-letters: the input can not contain any letters from 0 to 9
-- max: the maximum number of letters to be accepted as input. For this you have to pass value for `max` other wise the default max value is 30.
-- min: the minimum number of letters to be accepted as input. For this you have to pass value for `min` other wise the default min value is 3.
-- valid-email: the input should be a valid email address
+- **no-space:** the input can not contain spaces
+- **only-digits:** the input can contain only numbers from 0 to 9
+- **only-letters:** the input can not contain any letters from 0 to 9
+- **max:** the maximum number of letters to be accepted as input. For this you have to pass value for `max` other wise the default max value is 30.
+- **min:** the minimum number of letters to be accepted as input. For this you have to pass value for `min` other wise the default min value is 3.
+- **valid-email:** the input should be a valid email address
+
+## Custom Button
+
+### Props
+
+- **title:** *(string)* Button title
+- **onPressFn:** *(Function)* Function to be executed when you press the button
+- **style?:** Your own custom style that you want to apply on the button
+- **m?:** *(number)* margin of the button
+- **bc?:** *(string)* border color of the button
+- **bgc?:** *(string)* background color of the button
+- **br?:** *(number)* border radius of the button
+- **w?:** *(number)* width of the button
+- **sc?:** *(string)* shadow color of the button
+- **disabled?:** *(boolean)* disable value of the button 
+
+Note: Props with `?` are optional props.
+
