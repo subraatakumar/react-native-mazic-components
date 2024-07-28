08cd9f484d92d8bdbf691ac793acc4bf1aa727c0
subraatakumar <business.ttrc@gmail.com>
2024-07-28 21:23:58 +0530

    deploy cicd

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,40 +1,40 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'React Native Mazic Components',
-  tagline: '',
-  url: 'https://subrataindia.github.io/',
-  baseUrl: '/react-native-mazic-components/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "React Native Mazic Components",
+  tagline: "",
+  url: "https://subraatakumar.github.io/",
+  baseUrl: "/react-native-mazic-components/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'subrataindia', // Usually your GitHub org/user name.
-  projectName: 'react-native-mazic-componentss', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: "subraatakumar", // Usually your GitHub org/user name.
+  projectName: "react-native-mazic-componentss", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
         },
@@ -44,7 +44,7 @@ const config = {
           // Remove this to remove the "edit this page" links.
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -54,28 +54,32 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'RN Mazic Components',
+        title: "RN Mazic Components",
         logo: {
-          alt: 'RN Mazic Components',
-          src: 'img/logo.svg',
+          alt: "RN Mazic Components",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
           },
-          {href: 'https://codingstepbystep.ttrc.in/', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/subrataindia',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://codingstepbystep.ttrc.in/",
+            label: "Blog",
+            position: "left",
+          },
+          {
+            href: "https://github.com/subrataindia",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         copyright: `Copyright © ${new Date().getFullYear()} Subrata, India.`,
       },
       prism: {
